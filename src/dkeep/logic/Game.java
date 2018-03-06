16ea2cfcc6c821e0385ec243082b772014cf5230
Tiago Castro <tiagoaraujocastro@gmail.com>
2018-03-06 17:32:39 +0000

    code rearranging

--- src/dkeep/logic/Game.java
+++ src/dkeep/logic/Game.java
@@ -1,36 +1,94 @@
 package dkeep.logic;
 
+import dkeep.cli.UserInterface;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Game {
 
-    public enum State {
-        PLAY
-    }
-
     private ArrayList<Level> levels = new ArrayList<Level>();
-    private State state;
     private int level;
 
-    public Game(){level = 0;}
+    public Game(){
+        level = 0;
 
-    public ArrayList<Level> getLevels() {
-        return levels;
+        Character[][] map1 = new Character[][]{
+                {'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'},
+                {'X', ' ', ' ', ' ', 'I', ' ', 'X', ' ', ' ', 'X'},
+                {'X', 'X', 'X', ' ', 'X', 'X', 'X', ' ', ' ', 'X'},
+                {'X', ' ', 'I', ' ', 'I', ' ', 'X', ' ', ' ', 'X'},
+                {'X', 'X', 'X', ' ', 'X', 'X', 'X', ' ', ' ', 'X'},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X'},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X'},
+                {'X', 'X', 'X', ' ', 'X', 'X', 'X', 'X', ' ', 'X'},
+                {'X', ' ', 'I', ' ', 'I', ' ', 'X', ' ', ' ', 'X'},
+                {'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'}
+        };
+
+        Hero h = new Hero(1, 1);
+
+        levels.add(new Level(map1, h, 0));
+
+        Character[] route = new Character[]{'l', 'd', 'd', 'd', 'd', 'l', 'l', 'l', 'l', 'l', 'l', 'd', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'u', 'u', 'u', 'u', 'u'};
+        Guard g = new DrunkenGuard(1, 8, new ArrayList<Character>(Arrays.asList(route)));
+        levels.get(0).addGuard(g);
+
+        Door d1 = new Door(5, 0);
+        levels.get(0).addDoor(d1);
+        Door d2 = new Door(6, 0);
+        levels.get(0).addDoor(d2);
+
+        Lever l = new Lever(8, 7);
+        levels.get(0).addLever(l);
+
+        Character[][] map2 = new Character[][]{
+                {'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X'},
+                {'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X'},
+                {'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X'},
+                {'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X'},
+                {'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X'},
+                {'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X'},
+                {'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X'},
+                {'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'}
+        };
+
+        Hero h2 = new Hero(7, 1);
+        h2.arm();
+
+        levels.add(new Level(map2, h2, 1));
+
+        Door d3 = new Door(1, 0);
+        levels.get(1).addDoor(d3);
+
+        Key k = new Key(1, 7);
+        levels.get(1).addKey(k);
+
+        Ogre o = new Ogre(1, 4, 1, 3);
+        levels.get(1).addOgre(o);
+
+        Ogre o2 = new Ogre(3, 6, 3, 5);
+        levels.get(1).addOgre(o2);
+
     }
 
-    public void addLevel(Character[][] map, Hero h, int type){
-        levels.add(new Level(map, h, type));
+    public void start(){
+        for (Level lvl : levels) {
+            UserInterface.printMap(lvl.getMap());
+
+            if (!userMove())
+                break;
+
+            incLevel();
+        }
     }
 
-    public int getLevel() {
-        return level;
-    }
-
-    public void incLevel() {
+    private void incLevel() {
         level++;
     }
 
-    public boolean userMove() {
+    private boolean userMove() {
         return levels.get(level).userMove();
     }
 }
