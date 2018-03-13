4ebc5286bcf255a92007e0b678b52b008981279a
Tiago Castro <tiagoaraujocastro@gmail.com>
2018-03-13 18:08:19 +0000

    Revert "small change"
    This reverts commit 8a5e95ee3c389daf61cedb001bb2eb6bc1dc98d5.

--- src/dkeep/logic/Ogre.java
+++ src/dkeep/logic/Ogre.java
@@ -78,14 +78,14 @@ public class Ogre extends GameCharacter {
             symbol = '0';
         }
 
-        else if (map[next_x][next_y] == 'k') {
+        if (map[next_x][next_y] == 'k') {
             x=next_x;
             y=next_y;
             updateClub(map);
             symbol = '$';
         }
 
-        else if (map[next_x][next_y] == 'X' || map[next_x][next_y] == 'I' || map[next_x][next_y] == 'S') {
+        if (map[next_x][next_y] == 'X' || map[next_x][next_y] == 'I' || map[next_x][next_y] == 'S') {
             update(map);
         }
     }
