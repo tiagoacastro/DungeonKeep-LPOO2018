52f15543725668a0fa6c0bcada103b156b87689e
João Malheiro <jonas_malheiro_sousa@hotmail.com>
2018-04-03 19:00:19 +0100

    Images added and some menu frames

---
+++ src/dkeep/gui/MainMenu.java
@@ -0,0 +1,101 @@
+package dkeep.gui;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.*;
+import javax.imageio.*;
+import java.io.*;
+import java.awt.image.*;
+
+import dkeep.gui.MapEditor.charButtonPressed;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class MainMenu extends JPanel {
+
+	private JFrame menuWindow;
+	private JButton btnNewGame;
+	private JButton btnLoadGame;
+	private JButton btnExitGame;
+	private BufferedImage background;
+
+	//main function
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				MainMenu window = new MainMenu();
+			}
+		});
+	}
+
+	public MainMenu() {
+		
+		initializeFrame();
+		initializeButtons();
+	}
+	
+	public void initializeFrame() {
+		//frame
+		menuWindow = new JFrame("Dungeon Balls");
+		menuWindow.setBounds(100, 100, 640, 480);
+		menuWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		menuWindow.getContentPane().setLayout(gridBagLayout);
+	}
+	
+	public void initializeButtons() {
+		btnNewGame = new JButton("New Game");
+		btnNewGame.addActionListener(new newGameEvent());
+		GridBagConstraints gbc_btnNewGame = new GridBagConstraints();
+		gbc_btnNewGame.gridwidth = 3;
+		gbc_btnNewGame.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewGame.gridx = 9;
+		gbc_btnNewGame.gridy = 6;
+		menuWindow.getContentPane().add(btnNewGame, gbc_btnNewGame);
+		
+		btnLoadGame = new JButton("Load Game");
+		btnLoadGame.addActionListener(new loadGameEvent());
+		GridBagConstraints gbc_btnLoadGame = new GridBagConstraints();
+		gbc_btnLoadGame.gridwidth = 5;
+		gbc_btnLoadGame.insets = new Insets(0, 0, 5, 5);
+		gbc_btnLoadGame.gridx = 8;
+		gbc_btnLoadGame.gridy = 8;
+		menuWindow.getContentPane().add(btnLoadGame, gbc_btnLoadGame);
+		
+		btnExitGame = new JButton("Exit Game");
+		btnExitGame.addActionListener(new exitGameEvent());
+		GridBagConstraints gbc_btnExitGame = new GridBagConstraints();
+		gbc_btnExitGame.insets = new Insets(0, 0, 0, 5);
+		gbc_btnExitGame.gridx = 10;
+		gbc_btnExitGame.gridy = 11;
+		menuWindow.getContentPane().add(btnExitGame, gbc_btnExitGame);
+		menuWindow.setVisible(true);
+	}
+	
+	private class exitGameEvent implements ActionListener {
+		public void actionPerformed(ActionEvent arg0) {
+			menuWindow.dispatchEvent(new WindowEvent(menuWindow, WindowEvent.WINDOW_CLOSING));
+		}
+	}
+	
+	private class newGameEvent implements ActionListener {
+		public void actionPerformed(ActionEvent arg0) {
+		    SettingsMenu settings = new SettingsMenu();
+		}
+	}
+
+    private class loadGameEvent implements ActionListener {
+        public void actionPerformed(ActionEvent arg0) {
+        }
+    }
+}
---
+++ src/dkeep/gui/SettingsMenu.java
@@ -0,0 +1,108 @@
+package dkeep.gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+import dkeep.logic.Game;
+
+public class SettingsMenu {
+
+	private JFrame settingsMenu;
+	private JComboBox<String> guardType;
+	private JButton defaultKeep;
+	private JButton editKeep;
+	private JTextField ogres;
+	private Game game;
+	
+	public SettingsMenu() {
+
+		initializeFrame();
+
+		initializeGuardInput();
+		
+		initializeButtons();
+		
+		initializeOgreInput();
+	}
+
+	private void initializeFrame() {
+		settingsMenu = new JFrame("Game Settings");
+		settingsMenu.setBounds(100, 100, 300, 400);
+		settingsMenu.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		settingsMenu.getContentPane().setLayout(null);
+		settingsMenu.setVisible(true);
+	}
+
+	private void initializeButtons() {
+		defaultKeep = new JButton("Use default keep level");
+		defaultKeep.addActionListener(new defaultKeepEvent());
+		defaultKeep.setBounds(40, 274, 210, 21);
+		settingsMenu.getContentPane().add(defaultKeep);
+		
+		editKeep = new JButton("Edit the keep level");
+		editKeep.addActionListener(new editKeepEvent());
+		editKeep.setBounds(40, 300, 210, 21);
+		settingsMenu.getContentPane().add(editKeep);
+	}
+	
+	public void initializeGuardInput() {
+		//guard input
+		JLabel guardLabel = new JLabel();
+		guardLabel.setText("Guard personality");
+		guardLabel.setBounds(30, 30, 100, 20);
+		settingsMenu.getContentPane().add(guardLabel);
+
+		guardType = new JComboBox();
+		guardType.setBounds(135, 30, 100, 20);
+		guardType.addItem("Rookie");
+		guardType.addItem("Drunken");
+		guardType.addItem("Suspicious");
+		settingsMenu.getContentPane().add(guardType);
+	}
+	
+	public void initializeOgreInput() {
+		//ogre input
+		JLabel ogreLabel = new JLabel();
+		ogreLabel.setText("Number of Ogres");
+		ogreLabel.setBounds(30, 80, 100, 20);
+		settingsMenu.getContentPane().add(ogreLabel);
+
+		ogres = new JTextField(10);
+		ogres.setBounds(135, 80, 100, 20);
+		settingsMenu.getContentPane().add(ogres);
+	}
+
+	public void startGame() {
+
+		game = new Game();
+		game.loadLevel1(guardType.getSelectedItem().toString());
+
+		if (ogres.getText().equals("1") || ogres.getText().equals("2") || ogres.getText().equals("3") || ogres.getText().equals("4") || ogres.getText().equals("5")) {
+			game.loadLevel2(Integer.parseInt(ogres.getText()));
+			game.getLevel().draw();
+
+			DungeonKeepGUI gui = new DungeonKeepGUI();
+		} else {
+			JOptionPane.showMessageDialog(settingsMenu, "You have to insert a positive number of 5 or less!");
+		}
+	}
+
+	private class defaultKeepEvent implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+
+			startGame();
+			
+		}
+		
+	}
+
+	private class editKeepEvent implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+		}
+	}
+}
